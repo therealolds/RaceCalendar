b4992f021d4121245a8b354da03ac67f4a51648a
Olds <55405045+therealolds@users.noreply.github.com>
2025-11-04 21:45:53 +0100

    Add files via upload
    The time of races now takes into account local time. I have added a dark mode and other graphic changes.
    I have also added a section called 'Featured', where it duplicates the series flagged in the series.js (basically to have at the top the serie that I care about) and a function to hide some.

---
+++ series.js
@@ -0,0 +1,63 @@
+// Global series metadata used on the home page
+window.SERIES = [
+  {
+    id: 'f1',
+    name: 'Formula 1',
+    tag: 'motorsport',
+    json: 'calendars/f1.json',
+    page: 'pages/f1.html',
+    logo: 'logos/F1.png',
+    bgClass: 'bg-f1',
+    site: 'https://www.formula1.com/'
+  },
+  {
+    id: 'moto_gp',
+    name: 'Moto GP',
+    tag: 'motorsport',
+    json: 'calendars/moto_gp.json',
+    page: 'pages/moto_gp.html',
+    logo: 'logos/Moto_GP.png',
+    bgClass: 'bg-moto_gp',
+    site: 'http://www.motogp.com/'
+  },
+  {
+    id: 'wrc',
+    name: 'World Rally Championship',
+    tag: 'motorsport',
+    json: 'calendars/wrc.json',
+    page: 'pages/wrc.html',
+    logo: 'logos/wrc.svg',
+    bgClass: 'bg-wrc',
+    site: 'https://www.wrc.com/en'
+  },
+  {
+    id: 'wec',
+    name: 'World Endurance Championship',
+    tag: 'motorsport',
+    json: 'calendars/wec.json',
+    page: 'pages/wec.html',
+    logo: 'logos/wec.png',
+    bgClass: 'bg-wec',
+    site: 'https://www.fiawec.com/en/'
+  },
+  {
+    id: 'americas_cup',
+    name: "America's Cup",
+    tag: 'nautical',
+    json: 'calendars/americas_cup.json',
+    page: 'pages/americas_cup.html',
+    logo: 'logos/americas_cup_logo.png',
+    bgClass: 'bg-americascup',
+    site: 'https://www.americascup.com/'
+  },
+  {
+    id: 'gp_offshore',
+    name: "Grand Prix Off Shore",
+    tag: 'nautical',
+    json: 'calendars/gp_offshore.json',
+    page: 'pages/gp_offshore.html',
+    logo: 'logos/gp_offshore.png',
+    bgClass: 'bg-gp_offshore',
+    site: 'https://www.grandprixoffshorecittadicervia.it/'
+  }
+];
